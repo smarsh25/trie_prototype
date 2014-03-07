5bf3f2d51a539a39c8dd1d9046573fa808c8d4b6
smarsh25 <sjm.atl@gmail.com>
2014-03-07 12:38:01 -0800

    adding list found titles after each key press

--- public/app_view.js
+++ public/app_view.js
@@ -12,8 +12,21 @@ App.Views.Main = Backbone.View.extend({
   },
 
   updateSearch: function(event) {
-    var currTitle = $('#titleSearch').val();
+    // clear any previously listed titles
+    $('#titles').empty();
 
-    $('#titles').append('<li>'+currTitle+'</li>');    
+    currSearchTitle = $('#titleSearch').val();
+
+    // if a char has been entered, and not a search string exists, perform search
+    //   (this prevents backspacing to an empty string from listing all titles)
+    if (currSearchTitle.length > 0) {    
+      // get a list of titles for currently entered search title (each key)
+      var foundTitles = App.autocompleter.complete(currSearchTitle);
+
+      // list all the titles
+      foundTitles.forEach(function (title, index, array) {
+        $('#titles').append('<li>'+ title +'</li>');
+      });    
+    } 
   }
 });
