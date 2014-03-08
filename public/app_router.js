37520edf6ad7b55369a7bc0f68dc11125e1df29b
smarsh25 <sjm.atl@gmail.com>
2014-03-08 15:56:22 -0800

    adding support for any routes to hit main

--- public/app_router.js
+++ public/app_router.js
@@ -1,15 +1,11 @@
 App.Routers.Main = Backbone.Router.extend({
   routes: {
     "": "main",
-    "*other": "defaultRoute"
+    "*other": "main"  // send everything to main
   },
 
   main: function () {
     var view = new App.Views.Main();
     $('#container').html(view.render().el);
-  },
-
-  defaultRoute: function() {
-    console.log('unplanned route, sending to default.');
   }
 });
